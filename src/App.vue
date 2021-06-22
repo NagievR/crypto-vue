db4281180b0f86a755bc19f62ec04c1ff8927478
nagievr <ruslannagiev96@gmail.com>
2021-06-23 00:04:16 +0300

    some logic

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,9 @@
                 autocomplete="off"
                 class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
                 placeholder="Например DOGE"
+                v-model="tickerName"
+                @input="errors.nameExists = false"
+                @keydown.enter="addTicker()"
               />
             </div>
             <div 
@@ -32,12 +35,13 @@
                 {{ 'tiker' }}
               </span>
             </div>
-            <div class="text-sm text-red-600">Такой тикер уже добавлен</div>
+            <div class="text-sm text-red-600" v-if="errors.nameExists">Такой тикер уже добавлен</div>
           </div>
         </div>
         <button
           type="button"
           class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          @click="addTicker()"
         >
           <!-- Heroicon name: solid/mail -->
           <svg
@@ -56,24 +60,30 @@
         </button>
       </section>
 
-        <hr class="w-full border-t border-gray-600 my-4" />
+        <hr 
+          class="w-full border-t border-gray-600 my-4"
+          v-if="tickerList.length"
+        />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
 
           <div
-            :class="{ 'border-4 border-purple-800 border-solid': true }"
             class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
+            :class="{ 'border-4 border-purple-800 border-solid': false }"
+            v-for="item of tickerList"
+            :key="item.id"
           >
             <div class="px-4 py-5 sm:p-6 text-center">
               <dt class="text-sm font-medium text-gray-500 truncate">
-                {{ 'lol' }} - USD
+                {{ item.name }} - USD
               </dt>
               <dd class="mt-1 text-3xl font-semibold text-gray-900">
-                {{ 'lol' }}
+                {{ item.price }}
               </dd>
             </div>
             <div class="w-full border-t border-gray-200"></div>
             <button
               class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
+              @click="removeTicker(item.id)"
             >
               <svg
                 class="h-5 w-5"
@@ -107,9 +117,11 @@
 
       <section
         class="relative"
+        v-for="item of tickerList"
+        :key="item.id"
       >
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{'lol'}} - USD
+          {{ 'sds' }} - USD
         </h3>
         <div class="flex items-end border-gray-600 border-b border-l h-64">
           <div
@@ -154,24 +166,100 @@ export default {
 
   data() {
     return {
-
+      tickerName: '',
+      tickerList: [],
+      coinList: null,
+      errors: {
+        nameExists: false
+      },
     }
   },
 
   methods: {
-    // async requestData(tikerData) {
-    //   const API_KEY = 'a1c61f6ad994ee54a62c29e7108038ce055fea49db1853cf44995dc35d966b98';
-    //   const res = await fetch(
-    //     `https://min-api.cryptocompare.com/data/price?fsym=${tikerData.tiker}&tsyms=USD&api_key=${API_KEY}`
-    //   );
-    // },
+    requestData(tickerName) {
+      const API_KEY = 'a1c61f6ad994ee54a62c29e7108038ce055fea49db1853cf44995dc35d966b98';
 
+      const fetchPrice = async () => {
+        // ====================== process error ==================
+        const res = await fetch(
+          `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=${API_KEY}`
+        );
+        const data = await res.json();
+
+        const foundTicker = this.tickerList.find(obj => obj.name === tickerName);
+        if (foundTicker) {
+          foundTicker.price = data.USD;
+        }
+      };
+
+      setInterval(() => {
+        fetchPrice();
+      }, 5000);
+
+      // fetchPrice();
+    },
+
+    addTicker() {
+      if (!this.tickerName) {
+        alert('error');
+        return;
+      }
+
+      const nameFormatted = this.tickerName.toUpperCase();
+
+      const tickerExists = this.tickerList.find(obj => obj.name === nameFormatted);
+      if (tickerExists) {
+        this.errors.nameExists = true;
+        return;
+      }
+      
+      this.requestData(nameFormatted);
+
+      const newData = {
+        name: nameFormatted,
+        price: '...',
+        id: +new Date()
+      };
+
+      this.tickerName = '';
+      this.tickerList.push(newData);
+      this.updateTickersOnStorage();
+    },
+    removeTicker(id) {
+      this.tickerList = this.tickerList.filter(obj => obj.id !== id);
+      this.updateTickersOnStorage();
+    },
+    updateTickersOnStorage() {
+      const oldPriceRemoved = this.tickerList.map(obj => ({...obj, price: '...'}));
+      localStorage.setItem('tickerList', JSON.stringify(oldPriceRemoved));
+    },
 
   },
 
-  // watch
+  created() {
+    const storedTickers = localStorage.getItem('tickerList');
+    if (storedTickers) {
+      const parced = JSON.parse(storedTickers);
+      this.tickerList = parced;
+      this.tickerList.forEach(obj => this.requestData(obj.name));
+    }
 
-  mounted() {
+    const storedCoins = localStorage.getItem('coinList');
+    if (storedCoins) {
+      const parced = storedCoins.JSON.parse(storedCoins);
+      this.coinList = parced;
+    } else {
+      fetch('https://min-api.cryptocompare.com/data/all/coinlist?summary=true')
+        .then(res => res.json())
+        .then(list => this.coinList = list)
+        .catch(err => {
+          alert('try later');
+          console.log(err);
+        });
+    }
+
+    // fetch('https://min-api.cryptocompare.com/data/all/coinlist?summary=true')
+
 
   }
 
